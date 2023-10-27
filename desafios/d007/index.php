656f6cc6ba7d492c529036f19d172adb2ea435fa
Victor Almeida <145040590+ovictoralmeida@users.noreply.github.com>
2023-10-27 17:18:22 -0300

    Resolvendo desafio 007

---
+++ desafios/d007/index.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Desafio de PHP</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <main>
+        <h1>Informe seu salário</h1>
+        <form action="" method="post">
+            <label for="sal">Salário (R$)</label>
+            <input type="number" name="sal" id="sal">
+            <p>Considerando o salário mínimo de <strong>R$1.380,00</strong></p>
+            <input type="submit" value="Calcular">
+        </form>
+    </main>
+    <section>
+        <h2>Resultado Final</h2>
+    </section>
+</body>
+</html>
