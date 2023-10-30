f1d65e95ffb4b295b04ab13100d1711ae117b6c9
Victor Almeida <145040590+ovictoralmeida@users.noreply.github.com>
2023-10-30 18:25:31 -0300

    Desafio 008: Exercício das Raízes

---
+++ desafios/d008/index.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Desafio de PHP</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+
+<body>
+    <?php 
+        $numero = $_POST['num'];
+    ?>
+    <main>
+        <h1>Informe um número</h1>
+        <form action="<?=$_SERVER['PHP_SELF']?>" method="post">
+            <label for="num">Número</label>
+            <input type="number" name="num" id="num" value="<?=$numero?>">
+            <input type="submit" value="Calcular Raízes">
+        </form>
+    </main>
+    <section>
+        <h2>Resultado Final</h2>
+        <?php 
+            $raizQ = $numero ** (1/2);
+            $raizC = $numero ** (1/3);
+
+            echo "<p>Analisando o <strong>número $numero</strong>, temos:";
+            echo "<ul><li>A sua raiz quadrada é <strong>". number_format($raizQ, 3, ",", "."). "</strong></li>";
+            echo "<li>A sua raiz cúbica é <strong>". number_format($raizC, 3, ",", ".") ."<strong></li></ul>";
+        ?>
+    </section>
+</body>
+
+</html>
